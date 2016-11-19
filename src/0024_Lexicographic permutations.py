6fdc8478b415bb9134f67ce98cf353e73dc4b978
Don <huang26169@gmail.com>
2016-11-20 06:01:25 +0800

    Euler Project 24

--- src/0024_Lexicographic permutations.py
+++ src/0024_Lexicographic permutations.py
@@ -8,17 +8,23 @@
 #
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
-fact = lambda i:reduce(lambda x,y:x*y, range(1,i+1),1)
+
+# fact = lambda i:reduce(lambda x,y:x*y, range(1,i+1),1)
 
 def lexicographic_perm(num, list):
-    res_list = []
     length = len(list)
+
+    fact_list = [1]
+    for i in range(1, length):
+        fact_list.append(i*fact_list[i-1])
+
+    res_list = []
     for i in range(length,0,-1):
-        x = (num-1) / fact(i-1)
-        mod = (num-1) % fact(i-1)
+        x = (num-1) / fact_list[i-1]
+        mod = (num-1) % fact_list[i-1]
         res_list.append(list[x])
         list.remove(list[x])
-        num -= x*fact(i-1)
+        num -= x*fact_list[i-1]
     return reduce(lambda x,y:str(x)+str(y), res_list)
 
 print lexicographic_perm(1000000,range(10))
